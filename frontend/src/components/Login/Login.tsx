2d9395465f4f318678ea919d970145ba4f542973
Daniel Cambinda <danielcambinda@gmail.com>
2023-03-19 23:03:26 +0000

    login semi ready

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,10 +1,71 @@
-import React from "react";
+import React from "react"
+import { useNavigate} from "react-router-dom"
+import useAuth from "../../hooks/useAuth";
 
 const Login: React.FC = () => {
+
+    const navigate = useNavigate();
+    const userContext = useAuth();
+    const [email, setEmail] = React.useState<string>("");
+    const [password, setPassword] = React.useState<string>("");
+    const [loginNotOK, setLoginNotOK] = React.useState<boolean>(false);
+
+    const emailChangeHandler: React.ChangeEventHandler<HTMLInputElement> = ({target}: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(target.value);
+    }
+
+    const passwordChangeHandler: React.ChangeEventHandler<HTMLInputElement> = ({target}: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(target.value);
+    }
+
+    const submitHandler: React.FormEventHandler<HTMLFormElement> = async ({target,preventDefault}:React.FormEvent<HTMLFormElement>) => {
+        preventDefault()
+
+        const result = await fetch("/user/login", {
+            method: "POST",
+            body: JSON.stringify({
+              email,
+              password
+            }),
+            headers: {"Content-Type": "application/json"},
+          });
+      
+          if(result.status === 200) {
+            const {user} = await result.json();
+            userContext.setUser(user)
+
+            setEmail("");
+            setPassword("");
+            navigate('/profile')
+          }else {
+            setLoginNotOK(true)
+            setEmail("");
+            setPassword("");
+          }
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+    <div className="container text-center" style={{ maxWidth: 500 }}>
+        {loginNotOK && <p style={{"color":"red"}} >Email and&#47;or password incorrect&#40;s&#41; </p>}
+        <form onSubmit={submitHandler}>
+        
+            <h2 className="mb-5">Login</h2>
+            
+            <div className="form-outline mb-4">
+                <label className="form-label" htmlFor="form1Example1">Email</label>
+                <input type="email" id="form1Example1" className="form-control" value={email} onChange={emailChangeHandler} required/>
+            
+            </div>
+        
+            
+            <div className="form-outline mb-4">
+                <label className="form-label" htmlFor="form1Example2">Password</label>
+                <input type="password" id="form1Example2" className="form-control" value={password} onChange={passwordChangeHandler} required/>
+            </div>
+                
+            <button type="submit" className="btn btn-primary btn-block">Login</button>
+        </form>
+    </div>
     );
 }
 
